933f0e41b04502dbb48d88c884a0dd70fcd85caf
SoumyadeepB <soumyadeep.bh1994@gmail.com>
2019-05-16 14:01:40 +0200

    Plot changed

--- Classification/plotData.m
+++ Classification/plotData.m
@@ -3,9 +3,9 @@ function plotData(x,y)
 pos=find(y==1);
 neg=find(y==0);
 
-% plot3(x(pos,1),x(pos,2),y(pos,1),'rx','MarkerSize',6);
+% plot3(x(pos,1),x(pos,2),y(pos,1),'r+','MarkerSize',6);
 % hold on;
-% plot3(x(neg,1),x(neg,2),y(neg,1),'bo','MarkerSize',6);
+% plot3(x(neg,1),x(neg,2),y(neg,1),'b.','MarkerSize',6);
 
 plot(x(pos, 1), x(pos, 2), 'r+','MarkerSize',3)
 hold on
