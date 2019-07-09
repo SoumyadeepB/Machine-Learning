6961af6cdfc02ce30bcb4ba26bbcd8686bc54907
SoumyadeepB <soumyadeep.bh1994@gmail.com>
2019-07-09 22:45:40 +0200

    Gaussian Processes

---
+++ Gaussian Process/GaussianProcess.py
@@ -0,0 +1,27 @@
+'exec(%matplotlib inline)'
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+import scipy.sparse.linalg as sla
+import os
+from gaussian_processes_util import plot_gp
+
+def kernel(X1, X2, l=1.0, sigma_f=1.0):
+    ''' Isotropic squared exponential kernel. Computes a covariance matrix from points in X1 and X2. Args: X1: Array of m points (m x d). X2: Array of n points (n x d). Returns: Covariance matrix (m x n). '''
+    sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+    return sigma_f**2 * np.exp(-0.5 / l**2 * sqdist)
+
+
+
+# Finite number of points
+X = np.arange(-5, 5, 0.2).reshape(-1, 1)
+
+# Mean and covariance of the prior
+mu = np.zeros(X.shape)
+cov = kernel(X, X)
+
+# Draw three samples from the prior
+samples = np.random.multivariate_normal(mu.ravel(), cov, 3)
+
+# Plot GP mean, confidence interval and samples 
+plot_gp(mu, cov, X, samples=samples)
