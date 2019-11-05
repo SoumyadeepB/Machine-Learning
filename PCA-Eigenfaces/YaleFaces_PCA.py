d5c36ebf6e6639f5da6137c58513e1c2cf0a9446
Soumyadeep Bhattacharjee <soumyadeep.bh1994@gmail.com>
2019-11-05 23:14:25 +0100

    Update YaleFaces_PCA.py

--- PCA-Eigenfaces/YaleFaces_PCA.py
+++ PCA-Eigenfaces/YaleFaces_PCA.py
@@ -4,7 +4,7 @@ import scipy as sp
 import scipy.sparse.linalg as sla
 import os
 import time
-from tqdm import tqdm
+from tqdm import tqdm,tqdm_gui
 from PIL import Image 
 
 filepath = "./yalefaces/"
